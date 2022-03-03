cf3bdf7a4b971e41f851775230933584e6e2886f
komorab <komorabu@gmail.com>
2022-03-03 14:53:54 +0800

    isPrime.py

--- exercise/isPrime.py
+++ exercise/isPrime.py
@@ -1,5 +1,4 @@
 # 2022-2-22 用于判断一个数是否是质数
-import random
 
 
 def is_prime(num: int) -> bool:
@@ -14,24 +13,3 @@ def is_prime(num: int) -> bool:
                     return False
             return True
         return False
-
-
-def list_prime(num_list: list):
-    """
-    判断给定的数组中的每一项是否为质数
-    并返回由布尔值组成的数组
-    :rtype: list[bool]
-    """
-    value = []
-    for i in num_list:
-        if is_prime(i):
-            value.append(True)
-        else:
-            value.append(False)
-    return value
-
-
-if __name__ == '__main__':
-    num = [random.randint(2, 100000) for _ in range(20)]
-    num = [i for i in num if is_prime(i)]
-    print(num)

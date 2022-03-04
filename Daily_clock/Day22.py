8c01a07fc786d88ba17f68b4a61dc4cb82018566
komorab <komorabu@gmail.com>
2022-03-04 22:21:17 +0800

    Day22.py

---
+++ Daily_clock/Day22.py
@@ -0,0 +1,60 @@
+# 2022-3-4 2104 子数组范围和
+"""
+给你一个整数数组 nums 。nums 中，子数组的 范围 是子数组中最大元素和最小元素的差值。
+返回 nums 中 所有 子数组范围的 和 。
+子数组是数组中一个连续 非空 的元素序列。
+"""
+from math import inf
+
+
+class Solution:
+    @staticmethod  # O(n^2) O(1)
+    def subArrayRanges(self, nums: list[int]) -> int:
+        ans, n = 0, len(nums)
+        for i in range(n):
+            minVal, maxVal = inf, -inf
+            for j in range(i, n):
+                minVal = min(minVal, nums[j])
+                maxVal = max(maxVal, nums[j])
+                ans += maxVal - minVal
+        return ans
+
+    @staticmethod  # 单调栈方法 O(n) O(n)
+    def stack(self, nums: list[int]) -> int:
+        n = len(nums)
+        minLeft, maxLeft = [0] * n, [0] * n
+        minStack, maxStack = [], []
+        for i, num in enumerate(nums):
+            while minStack and nums[minStack[-1]] > num:
+                minStack.pop()
+            minLeft[i] = minStack[-1] if minStack else -1
+            minStack.append(i)
+
+            # 如果 nums[maxStack[-1]] == num, 那么根据定义，
+            # nums[maxStack[-1]] 逻辑上小于 num，因为 maxStack[-1] < i
+            while maxStack and nums[maxStack[-1]] <= num:
+                maxStack.pop()
+            maxLeft[i] = maxStack[-1] if maxStack else -1
+            maxStack.append(i)
+
+        minRight, maxRight = [0] * n, [0] * n
+        minStack, maxStack = [], []
+        for i in range(n - 1, -1, -1):
+            num = nums[i]
+            # 如果 nums[minStack[-1]] == num, 那么根据定义，
+            # nums[minStack[-1]] 逻辑上大于 num，因为 minStack[-1] > i
+            while minStack and nums[minStack[-1]] >= num:
+                minStack.pop()
+            minRight[i] = minStack[-1] if minStack else n
+            minStack.append(i)
+
+            while maxStack and nums[maxStack[-1]] < num:
+                maxStack.pop()
+            maxRight[i] = maxStack[-1] if maxStack else n
+            maxStack.append(i)
+
+        sumMax, sumMin = 0, 0
+        for i, num in enumerate(nums):
+            sumMax += (maxRight[i] - i) * (i - maxLeft[i]) * num
+            sumMin += (minRight[i] - i) * (i - minLeft[i]) * num
+        return sumMax - sumMin
